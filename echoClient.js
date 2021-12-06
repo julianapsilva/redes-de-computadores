470623d5546714103161292b6bb5006002314b3e
julianapsilva <julianadepaula001@gmail.com>
2021-12-06 14:39:23 -0300

    atualiza funcoes

--- echoClient.js
+++ echoClient.js
@@ -2,21 +2,22 @@ var socket = require('socket.io-client')('http://localhost:4444');
 const repl = require('repl')
 let username = null
 
-socket.on('disconnect', function () {
-    socket.emit('disconnect')
+socket.on('teste', function () {
+    // socket.emit('disconnect')
+    socket.disconnect()
+    process.exit()
+
 });
 socket.on('connect', () => {
     console.log('=== Conversa iniciada ===')
     username = process.argv[2]
 })
 socket.on('message', (data) => {
-    const { cmd, username } = data
-    console.log(username + ': ' + cmd.split('\n')[0]);
+    console.log('Resposta do servidor:', data);
 })
 repl.start({
     prompt: '',
     eval: (cmd) => {
-        console.log('chegou aq', cmd)
         socket.send({ cmd, username })
     }
 })
--- echoServer.js
+++ echoServer.js
@@ -1,13 +1,19 @@
 const http = require('http').createServer();
 const io = require('socket.io')(http);
 const port = 4444
+
 io.on('connection', (socket) => {
     console.log('connected')
     socket.on('message', (evt) => {
-        socket.emit('message', evt)
+        if(evt.cmd.startsWith('echo'))
+            socket.emit('message', evt.cmd.split(' ').pop())
+        else if (evt.cmd.startsWith('quit')) {
+            socket.emit('teste')
+            socket.disconnect()
+        }
     })
 })
-io.on('disconnect', (evt) => {
+io.on('teste', (evt) => {
     console.log('disconnected')
 })
 http.listen(port, () => console.log(`Servidor iniciado na porta: ${port}`))
